5e1b024a7e703d3017281a2a2d47610ce234f5cd
wldbrrt <klim4el@gmail.com>
2023-06-28 16:30:46 +0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
