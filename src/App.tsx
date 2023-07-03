7effe00fd8331113bcbb1c28e7db0101683c9d32
wldbrrt <klim4el@gmail.com>
2023-07-03 15:48:37 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' />
+          <Route path='/' element={<div>THE ROOT</div>} />
+          <Route path='/SignUp' element={<div>SIGN UP</div>} />
+          <Route path='/SignIn' element={<div> SIGN IN</div>} />
+          <Route path='/Fauvorites' element={<div> FAUFORITES</div>} />
+          <Route path='/History' element={<div>HISTORY </div>} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export { App }
