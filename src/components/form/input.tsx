184137ddccabb01d80f574673a45a7e14863fe7f
wldbrrt <klim4el@gmail.com>
2023-07-11 18:54:52 +0300

    add Input component

---
+++ src/components/form/input.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface InputProps {
+    input: object,
+    placeholder: string,
+    type: string,
+    meta: {
+        touched: boolean,
+        error: string,
+        warning: string | undefined
+    }
+
+}
+
+function Input({ input, placeholder, type, meta: { touched, error, warning } }: InputProps) {
+
+    return (
+        <div>
+            <label>{placeholder}</label>
+            <div>
+                <input {...input} placeholder={placeholder} type={type} />
+                {touched &&
+                    ((error && <span>{error}</span>) ||
+                        (warning && <span>{warning}</span>))}
+            </div>
+        </div>
+    )
+}
+
+export { Input }
