89872b729cbccb7236cb0b066d5e407aa05e13a2
wldbrrt <klim4el@gmail.com>
2023-07-12 19:40:15 +0300

    add search components

---
+++ src/components/search/search.tsx
@@ -0,0 +1,12 @@
+import { SearchInput } from "./searchInput";
+import React from "react";
+import './search.css';
+
+function Search() {
+    return (
+        <div>
+            <SearchInput />
+        </div>
+    )
+}
+export { Search };
---
+++ src/components/search/searchInput.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './searchInput.css';
+
+function SearchInput() {
+    return (
+        <input
+            type="text"
+
+        />
+    )
+}
+
+export { SearchInput };
