1a0003060a540e13d4220846a5bf4cf70d8e9079
wldbrrt <klim4el@gmail.com>
2023-07-14 12:45:06 +0300

    add searchInput

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,11 +1,21 @@
 import { SearchInput } from "./searchInput";
-import React from "react";
+import { GameList } from "./gameList";
+import { useDebounce } from "../../store/hooks";
+import React, { useState } from "react";
 import './search.css';
 
+
 function Search() {
+    const [gameName, setGameName] = useState('');
+    const debouncedValue = useDebounce(gameName, 800)
+
     return (
         <div>
-            <SearchInput />
+            <SearchInput onChangeHandler={setGameName} />
+            <button >
+                Search
+            </button>
+            <GameList pages={1} size={5} name={debouncedValue} />
         </div>
     )
 }
--- src/components/search/searchInput.tsx
+++ src/components/search/searchInput.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import './searchInput.css';
 
-function SearchInput() {
+interface SearchInputProps {
+    onChangeHandler: (gameName: string) => void
+}
+
+function SearchInput({ onChangeHandler }: SearchInputProps) {
+
     return (
         <input
             type="text"
-
+            placeholder="Enter game name"
+            name="search"
+            onChange={(e) => onChangeHandler(e.target.value)}
         />
     )
 }
