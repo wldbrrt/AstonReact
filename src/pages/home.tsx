16abf02a8d7fe7cb51500111bdfdf273af940584
wldbrrt <klim4el@gmail.com>
2023-07-14 12:50:29 +0300

    add Search to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,16 @@
 import { useAuthorization, useAppDispatch } from "../store/hooks";
+import { Search } from "../components/search/search";
 import { removeUser } from "../store/slices/user";
 import { Navigate } from "react-router-dom";
 import React from "react";
 
 function Home() {
-    const { isAuth, email } = useAuthorization();
-    const dispatch = useAppDispatch();
+    const { isAuth } = useAuthorization();
 
     return isAuth
         ? (
             <div>
+                <Search />
                 <h1>WELCOME</h1>
             </div>
         )
