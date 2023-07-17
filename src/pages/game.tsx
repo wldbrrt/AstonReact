9311f5e3ce80b324e4178476e41c067cf58c7c9f
wldbrrt <klim4el@gmail.com>
2023-07-17 16:12:16 +0300

    refactor: move game card to new component

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,31 +1,13 @@
-import { useAuthorization, useGameInfo } from '../store/hooks'
-import { useGetSingleGameQuery } from '../store/slices/gamesAPI'
+import { useAuthorization } from '../store/hooks'
+import { GameCard } from '../components/gameCard/gameCard'
 import { Navigate } from 'react-router-dom'
 import React from 'react'
 import './game.css'
 
 function Game() {
     const { isAuth } = useAuthorization()
-    const { id } = useGameInfo()
-    const { data } = useGetSingleGameQuery({ gameId: id })
 
-    console.log(data)
-
-    return isAuth ? (
-        <div className='game'>
-            <h2 className='game__name'>{data?.name}</h2>
-            <span className='game__rating'>Rating: {data?.rating}/5</span>
-            <span className='game__date'>Released date:{data?.released}</span>
-            <img
-                className='game__img'
-                src={data?.background_image}
-                alt={data?.name}
-            />
-            <span className='game__description'>{data?.description_raw}</span>
-        </div>
-    ) : (
-        <Navigate to='/SignIn' />
-    )
+    return isAuth ? <GameCard /> : <Navigate to='/SignIn' />
 }
 
 export { Game }
