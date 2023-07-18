d110a4f43a7f234f943e379a295fe9a19c32163e
wldbrrt <klim4el@gmail.com>
2023-07-19 01:08:28 +0300

    add history actions

--- src/store/slices/user.ts
+++ src/store/slices/user.ts
@@ -1,28 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     email: null,
     token: null,
-    id: null
-};
+    id: null,
+    history: null,
+}
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
         setUser(state, action) {
-            state.email = action.payload.email;
-            state.token = action.payload.token;
-            state.id = action.payload.id;
+            state.email = action.payload.email
+            state.token = action.payload.token
+            state.id = action.payload.id
         },
         removeUser(state) {
-            state.email = null;
-            state.token = null;
-            state.id = null;
-        }
-    }
-});
+            state.email = null
+            state.token = null
+            state.id = null
+            state.history = null
+        },
+        setUserHistory(state, action) {
+            state.history = action.payload.history
+        },
+        removeUserHistory(state) {
+            state.history = null
+        },
+    },
+})
 
-export const { setUser, removeUser } = userSlice.actions;
+export const { setUser, removeUser, setUserHistory, removeUserHistory } =
+    userSlice.actions
 
 export default userSlice.reducer
