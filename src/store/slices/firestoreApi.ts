96d4f3e2055df86599ddcbeb3a2e9511e65bb141
wldbrrt <klim4el@gmail.com>
2023-07-20 23:59:21 +0300

    feature: add favorites queries

--- src/store/slices/firestoreApi.ts
+++ src/store/slices/firestoreApi.ts
@@ -1,6 +1,14 @@
 import { database } from '../../firebase'
-import { arrayUnion, doc, updateDoc, getDoc, setDoc } from 'firebase/firestore'
+import {
+    arrayUnion,
+    doc,
+    updateDoc,
+    getDoc,
+    setDoc,
+    deleteField,
+} from 'firebase/firestore'
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
+import { FormMethod } from 'react-router-dom'
 
 interface IHistory {
     searchReq: string
@@ -10,6 +18,30 @@ interface IHistory {
 interface IUserData {
     email: string | null
     history: IHistory[]
+}
+
+interface IUserNestedObj {
+    name: string
+    background_image: string
+    date: string
+    id: number
+}
+
+interface IUserFavorites {
+    [id: string]: IUserNestedObj
+}
+
+interface IUserFavoritesProps {
+    email: string | null
+    id: string
+    name: string
+    date: string
+    background_image: string
+}
+
+interface IUserDelParams {
+    email: string | null
+    id: string
 }
 
 interface IHistoryQueryParams {
@@ -58,6 +90,7 @@ export const firestoreApi = createApi({
                         {
                             email: email,
                             history: [],
+                            favorites: {},
                         }
                     )
                         .then(() => true)
@@ -92,6 +125,75 @@ export const firestoreApi = createApi({
                 },
             }
         ),
+        getUserFavorites: builder.query<IUserFavorites, IHistoryQueryParams>({
+            async queryFn({ email: email }) {
+                try {
+                    const ref = await getDoc(
+                        doc(database, 'Users', `${email}`)
+                    ).then(res => {
+                        if (res.exists()) {
+                            return {
+                                ...res.data().favorites,
+                            }
+                        }
+                    })
+                    return { data: ref }
+                } catch (error: any) {
+                    return { error: error.message }
+                }
+            },
+        }),
+        updateUserFavorites: builder.query<
+            IUserUpdatedData,
+            IUserFavoritesProps
+        >({
+            async queryFn({
+                email: email,
+                name: name,
+                date: date,
+                background_image: background_image,
+                id: id,
+            }) {
+                try {
+                    const ref = await updateDoc(
+                        doc(database, 'Users', `${email}`),
+                        {
+                            [`favorites.${[id]}`]: {
+                                email: email,
+                                name: name,
+                                date: date,
+                                background_image: background_image,
+                                id: id,
+                            },
+                        }
+                    )
+                        .then(() => true)
+                        .catch(() => false)
+
+                    return { data: { isSuccess: ref } }
+                } catch (error: any) {
+                    return { error: error.message }
+                }
+            },
+        }),
+        deleteUserFavorites: builder.query<IUserUpdatedData, IUserDelParams>({
+            async queryFn({ email: email, id: id }) {
+                try {
+                    const ref = await updateDoc(
+                        doc(database, 'Users', `${email}`),
+                        {
+                            [`favorites.${[id]}`]: deleteField(),
+                        }
+                    )
+                        .then(() => true)
+                        .catch(() => false)
+
+                    return { data: { isSuccess: ref } }
+                } catch (error: any) {
+                    return { error: error.message }
+                }
+            },
+        }),
     }),
 })
 
@@ -101,4 +203,10 @@ export const {
     useLazySetUserHistoryQuery,
     useLazyUpdateUserHistoryQuery,
     useLazyGetUserHistoryQuery,
+    useGetUserFavoritesQuery,
+    useLazyGetUserFavoritesQuery,
+    useUpdateUserFavoritesQuery,
+    useLazyUpdateUserFavoritesQuery,
+    useDeleteUserFavoritesQuery,
+    useLazyDeleteUserFavoritesQuery,
 } = firestoreApi
