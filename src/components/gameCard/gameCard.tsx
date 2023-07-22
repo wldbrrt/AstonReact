71ee4821e719252142617a0dac122307432d28f8
wldbrrt <klim4el@gmail.com>
2023-07-22 22:04:54 +0300

    fix: favorites add with unlogged user

--- src/components/gameCard/gameCard.tsx
+++ src/components/gameCard/gameCard.tsx
@@ -19,24 +19,104 @@ function GameCard({ gameId }: IgameCardProps) {
     const { email, isAuth } = useAuthorization()
     const { data, isLoading, isFetching, isSuccess, isError, error } =
         useGetSingleGameQuery({ gameId: gameId })
-    const { data: dataFav, isSuccess: isSuccessFav } = useGetUserFavoritesQuery(
-        { email: email }
-    )
 
     const [trigger] = useLazyUpdateUserFavoritesQuery()
     const [triggerGet] = useLazyGetUserFavoritesQuery()
 
     let content
 
-    if (isLoading || isFetching) {
-        content = <Loader />
-    } else if (isSuccess && isSuccessFav) {
-        const dataArrFav = Object.values(dataFav)
-        const IdsArr = dataArrFav.map(e => {
-            return Number(e.id)
-        })
-        const isInFavorites = IdsArr.includes(gameId)
+    if (isAuth) {
+        const { data: dataFav, isSuccess: isSuccessFav } =
+            useGetUserFavoritesQuery({ email: email })
 
+        if (isLoading || isFetching) {
+            content = <Loader />
+        } else if (isSuccess && isSuccessFav) {
+            const dataArrFav = Object.values(dataFav)
+            const IdsArr = dataArrFav.map(e => {
+                return Number(e.id)
+            })
+            const isInFavorites = IdsArr.includes(gameId)
+
+            content = (
+                <div className='game'>
+                    <h2 className='game__name'>{data.name}</h2>
+                    <span className='game__rating'>
+                        Rating: {data.rating}/5
+                    </span>
+                    <span className='game__date'>
+                        Released date:{data.released}
+                    </span>
+                    <img
+                        className='game__img'
+                        src={data.background_image}
+                        alt={data.name}
+                    />
+                    <button
+                        disabled={!isAuth || isInFavorites}
+                        className='game__add'
+                        onClick={() => {
+                            trigger({
+                                id: String(gameId),
+                                email: email,
+                                name: data.name,
+                                background_image: data.background_image,
+                                date: getCurrentDate(),
+                            })
+                            triggerGet({ email: email })
+                        }}
+                    >
+                        Add to Favorite
+                    </button>
+                    <span className='game__description'>
+                        {data.description_raw}
+                    </span>
+                </div>
+            )
+        } else if (isSuccess) {
+            content = (
+                <div className='game'>
+                    <h2 className='game__name'>{data.name}</h2>
+                    <span className='game__rating'>
+                        Rating: {data.rating}/5
+                    </span>
+                    <span className='game__date'>
+                        Released date:{data.released}
+                    </span>
+                    <img
+                        className='game__img'
+                        src={data.background_image}
+                        alt={data.name}
+                    />
+                    <button
+                        disabled={!isAuth}
+                        className='game__add'
+                        onClick={() => {
+                            trigger({
+                                id: String(gameId),
+                                email: email,
+                                name: data.name,
+                                background_image: data.background_image,
+                                date: getCurrentDate(),
+                            })
+                            triggerGet({ email: email })
+                        }}
+                    >
+                        Add to Favorite
+                    </button>
+                    <span className='game__description'>
+                        {data.description_raw}
+                    </span>
+                </div>
+            )
+        } else if (isError) {
+            content = <div>{error.toString()}</div>
+        }
+
+        return <div>{content}</div>
+    } else if (isLoading || isFetching) {
+        content = <Loader />
+    } else if (isSuccess) {
         content = (
             <div className='game'>
                 <h2 className='game__name'>{data.name}</h2>
@@ -50,7 +130,7 @@ function GameCard({ gameId }: IgameCardProps) {
                     alt={data.name}
                 />
                 <button
-                    disabled={!isAuth || isInFavorites}
+                    disabled={true}
                     className='game__add'
                     onClick={() => {
                         trigger({
